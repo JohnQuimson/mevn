400c7308bc44acdd67bde5fb1b3ef9e07ba4cb13
JohnQuimson <john.quimson777@gmail.com>
2024-10-04 22:19:30 +0200

    feat: define routes

---
+++ src/views/ProductDetailPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>This is the product detail</h1>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetail',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
