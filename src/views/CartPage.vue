ecaccb81bbad395dc70e410eeeddd6dc55f7db41
JohnQuimson <john.quimson777@gmail.com>
2024-10-05 21:40:22 +0200

    feat: cart

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -1,12 +1,40 @@
-<template>
-  <h1>This is the Cart page</h1>
-</template>
-
 <script>
+import { cartItems } from '@/fake-data';
+
 export default {
   name: 'CartPage',
+  data() {
+    return {
+      cartItems,
+    };
+  },
+  computed: {
+    totalPrice() {
+      return this.cartItems.reduce((sum, item) => sum + Number(item.price), 0);
+    },
+  },
 };
 </script>
+
+<template>
+  <div id="page-wrap">
+    <h1>Shopping Cart</h1>
+    <div
+      v-for="product in cartItems"
+      :key="product.id"
+      class="product-container"
+    >
+      <img class="product-image" :src="product.imageUrl" alt="" />
+      <div class="details-wrap">
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.price }}€</p>
+      </div>
+      <button class="remove-button">Remove from Cart</button>
+    </div>
+    <h3 id="total-price">Total: {{ totalPrice }}€</h3>
+    <button id="checkout-button">Proceed to Checkout</button>
+  </div>
+</template>
 
 <style scoped>
 h1 {
@@ -23,5 +51,29 @@ h1 {
 
 #checkout-button {
   width: 100%;
+}
+
+.product-container {
+  align-content: 'center';
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  padding: 16px;
+  width: 100%;
+}
+
+.product-image {
+  flex: 1;
+  height: 100px;
+  max-width: 100px;
+}
+
+.details-wrap {
+  padding: 0 16px;
+  flex: 3;
+}
+
+.remove-button {
+  flex: 1;
+  margin: auto;
 }
 </style>
